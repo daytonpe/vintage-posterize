eb379947842675d5224b0601a6b632601c0c8d8f
Pat Dayton <daytonpe@Pats-MacBook-Pro.local>
2018-11-28 00:29:27 -0600

    update to zermatt2

--- run.sh
+++ run.sh
@@ -48,31 +48,59 @@
 # python3 neural_style.py eval --content-image ./images/oregon/summit.jpg --model   ./models/vintage_zion_10k_2e.model --output-image ./outputs/zion_10k_2e/summit.jpg --cuda 0 --content-scale 4
 
 # /other
-# python3 neural_style.py eval --content-image ./images/other/flags.jpg --model ./models/vintage_zion_10k_2e.model --output-image ./outputs/zion_10k_2e/flags.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/other/flags.jpg --model ./models/vintage_zion_10k_2e.model --output-image ./outputs/zion_10k_2e/flags.jpg --cuda 0 --content-scale 4
+
 
 ##################################################################################################################################################################################################################
 
-# ZERMATT
+# ZERMATT 1
 # /washington
-python3 neural_style.py eval --content-image ./images/washington/muir1.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir1.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/washington/muir2.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir2.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/washington/muir3.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir3.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/washington/muir4.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir4.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/washington/muir5.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir5.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/washington/rainier.jpg --model ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/rainier.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/washington/leavenworth.jpg --model ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/leavenworth.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/washington/muir1.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir1.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/washington/muir2.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir2.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/washington/muir3.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir3.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/washington/muir4.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir4.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/washington/muir5.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/muir5.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/washington/rainier.jpg --model ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/rainier.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/washington/leavenworth.jpg --model ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/leavenworth.jpg --cuda 0 --content-scale 4
 
 # /california
-python3 neural_style.py eval --content-image ./images/california/elcap1.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/elcap1.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/california/elcap2.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/elcap2.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/california/halfdome1.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/halfdome1.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/california/halfdome2.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/halfdome2.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/california/halfdome3.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/halfdome3.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/california/elcap1.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/elcap1.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/california/elcap2.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/elcap2.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/california/halfdome1.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/halfdome1.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/california/halfdome2.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/halfdome2.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/california/halfdome3.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/halfdome3.jpg --cuda 0 --content-scale 4
 
 # /oregon
-python3 neural_style.py eval --content-image ./images/oregon/ascent.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/ascent.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/oregon/hood.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/hood.jpg --cuda 0 --content-scale 4
-python3 neural_style.py eval --content-image ./images/oregon/summit.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/summit.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/oregon/ascent.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/ascent.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/oregon/hood.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/hood.jpg --cuda 0 --content-scale 4
+# python3 neural_style.py eval --content-image ./images/oregon/summit.jpg --model   ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/summit.jpg --cuda 0 --content-scale 4
 
 # /other
-python3 neural_style.py eval --content-image ./images/other/flags.jpg --model ./models/vintage_zion_10k_2e.model --output-image ./outputs/zion_10k_2e/flags.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/other/flags.jpg --model ./models/vintage_zermatt_10k_2e.model --output-image ./outputs/zermatt_10k_2e/flags.jpg --cuda 0 --content-scale 4
+
+##################################################################################################################################################################################################################
+
+# ZERMATT 2
+# /washington
+python3 neural_style.py eval --content-image ./images/washington/muir1.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/muir1.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/washington/muir2.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/muir2.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/washington/muir3.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/muir3.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/washington/muir4.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/muir4.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/washington/muir5.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/muir5.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/washington/rainier.jpg --model ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/rainier.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/washington/leavenworth.jpg --model ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/leavenworth.jpg --cuda 0 --content-scale 4
+
+# /california
+python3 neural_style.py eval --content-image ./images/california/elcap1.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/elcap1.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/california/elcap2.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/elcap2.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/california/halfdome1.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/halfdome1.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/california/halfdome2.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/halfdome2.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/california/halfdome3.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/halfdome3.jpg --cuda 0 --content-scale 4
+
+# /oregon
+python3 neural_style.py eval --content-image ./images/oregon/ascent.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/ascent.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/oregon/hood.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/hood.jpg --cuda 0 --content-scale 4
+python3 neural_style.py eval --content-image ./images/oregon/summit.jpg --model   ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/summit.jpg --cuda 0 --content-scale 4
+
+# /other
+python3 neural_style.py eval --content-image ./images/other/flags.jpg --model ./models/vintage_zermatt2_10k_2e.model --output-image ./outputs/zermatt2_10k_2e/flags.jpg --cuda 0 --content-scale 4
